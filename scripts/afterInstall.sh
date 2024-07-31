67b04a61cfdbdb558ded3bbd3ed144ad4333763b
sigitechofficial <sigidevelopers@gmail.com>
2024-07-31 15:54:24 +0500

    add bashscript files for blue & green D

---
+++ scripts/afterInstall.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Install Node.js 16
+curl -sL https://rpm.nodesource.com/setup_16.x | sudo bash -
+sudo yum install -y nodejs
+
+# Install PM2
+sudo npm install -g pm2
+
+# Navigate to the project directory
+cd /home/ubuntu/fomino
+
+# Install project dependencies
+npm install
---
+++ scripts/applicationStart.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# Navigate to the project directory
+cd /home/ubuntu/fomino
+
+# Start the application using PM2
+pm2 start fomino.js
+
